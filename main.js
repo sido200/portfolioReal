a4127ae2d6ab1386ebae29b04db74337a84c0f47
sido200 <93400204+sido200@users.noreply.github.com>
2024-01-28 16:18:11 +0100

    sidali

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const nav=document.querySelector(".navr");
 const btn=document.querySelector(".fa-bars");
+const html=document.querySelector("html");
 const nav2=document.querySelector("nav");
 btn.addEventListener('click',()=>{
 nav.classList.add("active-bar")
+
+html.classList.add("stop-scrolling");
 
 })
 const btn2=document.querySelector(".navr i");
 btn2.addEventListener('click',()=>{
     nav.classList.remove("active-bar")
+    html.classList.remove("stop-scrolling");
 
 
 })
@@ -16,6 +20,7 @@ document.addEventListener('click', function(event) {
     if (!(nav2).contains(event.target)) {
       // Do something when clicked outside the target element
       nav.classList.remove("active-bar")
+      html.classList.remove("stop-scrolling");
     }
   });
 
